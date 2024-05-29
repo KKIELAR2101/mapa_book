eab1203774fa56bba434446461de1bb1a314e0e6
KKIELAR2101 <167193874+KKIELAR2101@users.noreply.github.com>
2024-05-29 23:45:06 +0200

    wip: add a menu

--- main.py
+++ main.py
@@ -10,4 +10,14 @@ def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twój znajimy {user['name']} opublikował: {user['posts']} postów ")
 
-show_users(users)
+if __name__ == "__main__":
+    print("Witaj uzytkowniku")
+    while True:
+        print("Menu:")
+        print("1. Wyświetl co u  znajomych")
+        menu_option: str = input("Dokonaj wyboru")
+        if menu_option == "0":
+            print("Program kończy prace")
+            break
+        if menu_option == "1":
+            show_users(users)
